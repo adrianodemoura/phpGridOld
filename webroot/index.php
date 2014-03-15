98c7d2bea2e8c625820272eca2a07fa014eb338b
Adriano Carneiro de Moura <adrianodemoura@gmail.com>
2014-03-15 19:50:56 -0300

    voltando com o index do webroot.

---
+++ webroot/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Arquivo index do sistema
+ * 
+ */
+	// iniciando o cronômetro
+	define('INICIO',microtime(true));
+
+	// configurando o ambiente
+	define('AMBIENTE','DESENVOLVIMENTO');
+	//define('AMBIENTE','HOMOLOGAÇÃO');
+	//define('AMBIENTE','PRODUÇÃO');
+
+	// constantes locais
+	define('APP','../');
+	define('CORE','../Lib/Core/');
+
+	// incluindo a view de saída
+	require_once(CORE.'Boot.php');
+	$Boot = new Boot();
+
+	// saída
+	echo $Boot->render();
