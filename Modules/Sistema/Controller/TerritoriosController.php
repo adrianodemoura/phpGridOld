e27472ba71c6e03dcbe9cc236d6a092795d91397
ThiagoSouzaMoura <thiagodesouzamoura@gmail.com>
2014-03-16 17:54:31 -0300

    correção no lista core.

---
+++ Modules/Sistema/Controller/TerritoriosController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class Territórios
+ * 
+ * @package			Sistema
+ * @subpackage		Sistema.Controller
+ */
+/**
+ * Include files
+ */
+include_once(APP.'Modules/Sistema/Controller/SistemaAppController.php');
+class TerritoriosController extends SistemaAppController {
+	/**
+	 * Model Territorio
+	 * 
+	 * @var		array
+	 */
+	public $Model = array('Territorio');
+}
---
+++ Modules/Sistema/Model/Territorio.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Class Territorio
+ * 
+ * @package		Sistema
+ * @package		Sistema.Model
+ */
+/**
+ * Include files
+ */
+require_once(APP.'Modules/Sistema/Model/SistemaAppModel.php');
+class Territorio extends SistemaAppModel {
+	/**
+	 * Nome da tabela de cidades
+	 * 
+	 * @var		string	
+	 * @access	public
+	 */
+	public $tabela		= 'territorios';
+
+	/**
+	 * Chave primária do model usuários
+	 * 
+	 * @var		array
+	 * @access	public
+	 */
+	public $primaryKey 	= array('id');
+
+	/**
+	 * Nickname para a tabela usuarios
+	 * 
+	 * @var		string
+	 * @access	public
+	 */
+	public $alias		= 'Territorio';
+
+	/**
+	 * Propriedade de cada campo da tabela usuários
+	 * 
+	 * @var		array
+	 * @acess	public
+	 */
+	public $esquema = array
+	(
+		'id'		=> array
+		(
+			'tit'	=> 'Id',
+		),
+		'nome'		=> array
+		(
+			'tit'	=> 'Nome',
+		)
+	);
+}
